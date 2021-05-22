f6d0e77da8a4944807d6d2fa27d30eb925375b27
Richo Ha <richard.dqh@gmail.com>
2021-05-22 16:07:48 +0800

    Got server to send through html code

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,8 +1,18 @@
 // Dependencies
-const express = require('express');
-
+const express = require('express')
+const port = 3000
 const app = express();
-const PORT = 3000;
+const path = require('path');
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+});
 
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+//Set up server to listen on the provided port number.
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
+
